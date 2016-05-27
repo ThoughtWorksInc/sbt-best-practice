2024d6d3d4e4e6039581091be7e88040e7dfc30d
Yang Bo <atryyang@thoughtworks.com>
2016-05-28 06:26:51 +0800

    Add isGitDir setting and scmInfo setting

--- project/src/main/scala/com/thoughtworks/sbtBestPractice/GitInformation.scala
+++ project/src/main/scala/com/thoughtworks/sbtBestPractice/GitInformation.scala
@@ -1,8 +1,11 @@
 package com.thoughtworks.sbtBestPractice
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.ConfigConstants._
+import org.eclipse.jgit.lib.RepositoryBuilder
 import sbt._
 import sbt.Keys._
+
 import scala.collection.JavaConverters._
 
 /**
@@ -12,18 +15,57 @@ object GitInformation extends AutoPlugin {
 
   override def trigger = allRequirements
 
+  object autoImport {
+
+    val isGitDir = SettingKey[Boolean]("is-git-dir", "Determine if current project directory is a git work copy")
+
+  }
+
+  import autoImport._
+
   override def projectSettings = Seq(
+    isGitDir := {
+      (new RepositoryBuilder).findGitDir(baseDirectory.value).getGitDir != null
+    },
+    scmInfo := {
+      if (isGitDir.value) {
+        val git = Git.open(baseDirectory.value)
+        try {
+          val remoteName = git.getRepository.getConfig.getString(CONFIG_BRANCH_SECTION, git.getRepository.getBranch, CONFIG_KEY_REMOTE)
+          val Some(remote) = git.remoteList().call().asScala.find(_.getName == remoteName)
+          val url = remote.getURIs.asScala.head
+          Some(ScmInfo(
+            new URL("https", url.getHost, url.getPath match {
+              case abstractPath if abstractPath.startsWith("/") =>
+                abstractPath
+              case relativePath =>
+                raw"""/$relativePath"""
+            }),
+            url.toString,
+            None
+          ))
+        } finally {
+          git.close()
+        }
+      } else {
+        None
+      }
+    },
     developers := {
-      val git = Git.open(baseDirectory.value)
-      try {
-        (for {
-          commit <- git.log().call().asScala
-        } yield {
-          val author = commit.getAuthorIdent
-          Developer("", author.getName, author.getEmailAddress, new java.net.URL("mailto", null, author.getEmailAddress))
-        }).toSet.toList
-      } finally {
-        git.close()
+      if (isGitDir.value) {
+        val git = Git.open(baseDirectory.value)
+        try {
+          (for {
+            commit <- git.log().call().asScala
+          } yield {
+            val author = commit.getAuthorIdent
+            Developer("", author.getName, author.getEmailAddress, new java.net.URL("mailto", null, author.getEmailAddress))
+          }).toSet.toList
+        } finally {
+          git.close()
+        }
+      } else {
+        Nil
       }
     }
   )
