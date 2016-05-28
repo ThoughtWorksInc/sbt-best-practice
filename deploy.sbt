719019681cf2d1f7f6652c759cab508d48cc9904
Yang Bo <atryyang@thoughtworks.com>
2016-05-29 02:34:23 +0800

    Specify secret sbt file name

--- deploy.sbt
+++ deploy.sbt
@@ -1,6 +1,6 @@
 enablePlugins(TravisEnvironmentVariables)
 
 sys.env.get("SECRET_GIT") match {
-  case Some(gitUri) => Some(addSbtFileFromGit(gitUri))
+  case Some(gitUri) => Some(addSbtFileFromGit(gitUri, file("secret.sbt")))
   case None => None
 }
--- remote-sbt-file/src/main/scala/com/thoughtworks/sbtBestPractice/remoteSbtFile/RemoteSbtFile.scala
+++ remote-sbt-file/src/main/scala/com/thoughtworks/sbtBestPractice/remoteSbtFile/RemoteSbtFile.scala
@@ -4,6 +4,7 @@ import com.thoughtworks.sbtBestPractice.issue2514.DslEntries
 import com.twitter.common.io.FileUtils
 import org.eclipse.jgit.api.Git
 import sbt._
+import java.io.File
 
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
@@ -11,15 +12,32 @@ import sbt._
 object RemoteSbtFile extends AutoPlugin {
 
   object autoImport {
-    def addSbtFileFromGit(gitUri:String) = {
+
+    def addAllSbtFilesFromGit(gitUri: String) = {
       val secretDirectory = FileUtils.createTempDir
       Git.cloneRepository().
         setURI(gitUri).
         setDirectory(secretDirectory).
         call().
         close()
-      DslEntries.autoImport.addSbtFile((secretDirectory * "*.sbt").get: _*)
+      DslEntries.autoImport.addSbtFile((secretDirectory ** "*.sbt").get: _*)
     }
+
+    def addSbtFileFromGit(gitUri: String, relativeSbtFiles: File*) = {
+      val secretDirectory = FileUtils.createTempDir
+      Git.cloneRepository().
+        setURI(gitUri).
+        setDirectory(secretDirectory).
+        call().
+        close()
+
+      val resolvedFiles = for {
+        relativeSbtFile <- relativeSbtFiles
+      } yield (secretDirectory / relativeSbtFile.toString)
+
+      DslEntries.autoImport.addSbtFile(resolvedFiles: _*)
+    }
+
   }
 
 }
