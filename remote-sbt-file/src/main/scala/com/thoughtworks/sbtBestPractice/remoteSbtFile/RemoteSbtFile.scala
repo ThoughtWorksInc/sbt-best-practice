4f264f50a1bd625ba575e661b30848c2b1ea7bc9
Yang Bo <atryyang@thoughtworks.com>
2016-05-31 11:43:36 +0800

    Support credentials when addSbtFilesFromGit

--- remote-sbt-file/src/main/scala/com/thoughtworks/sbtBestPractice/remoteSbtFile/RemoteSbtFile.scala
+++ remote-sbt-file/src/main/scala/com/thoughtworks/sbtBestPractice/remoteSbtFile/RemoteSbtFile.scala
@@ -5,6 +5,9 @@ import org.eclipse.jgit.api.Git
 import sbt._
 import java.io.File
 
+import org.eclipse.jgit.transport.CredentialsProvider
+import sbt.internals.ProjectManipulation
+
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
@@ -28,17 +31,26 @@ object RemoteSbtFile extends AutoPlugin {
       }
     }
 
-    def addAllSbtFilesFromGit(gitUri: String) = {
+    def addAllSbtFilesFromGit(gitUri: String): ProjectManipulation = {
+      addAllSbtFilesFromGit(gitUri, CredentialsProvider.getDefault)
+    }
+
+    def addAllSbtFilesFromGit(gitUri: String, credential: CredentialsProvider): ProjectManipulation = {
       val secretDirectory = IO.createTemporaryDirectory
       Git.cloneRepository().
         setURI(gitUri).
         setDirectory(secretDirectory).
+        setCredentialsProvider(credential).
         call().
         close()
       DslEntries.autoImport.addSbtFiles((secretDirectory ** "*.sbt").get: _*)
     }
 
-    def addSbtFilesFromGit(gitUri: String, relativeSbtFiles: File*) = {
+    def addSbtFilesFromGit(gitUri: String, relativeSbtFiles: File*): ProjectManipulation = {
+      addSbtFilesFromGit(gitUri, CredentialsProvider.getDefault, relativeSbtFiles: _*)
+    }
+
+    def addSbtFilesFromGit(gitUri: String, credential: CredentialsProvider, relativeSbtFiles: File*): ProjectManipulation = {
       val secretDirectory = IO.createTemporaryDirectory
       Git.cloneRepository().
         setURI(gitUri).
