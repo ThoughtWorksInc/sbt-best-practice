7643382a480c3c9d319afb6cb1c8a4a19e59f2b2
Yang Bo <atryyang@thoughtworks.com>
2016-06-01 11:54:00 +0800

    Add missing requires

--- git/src/main/scala/com/thoughtworks/sbtBestPractice/git/Git.scala
+++ git/src/main/scala/com/thoughtworks/sbtBestPractice/git/Git.scala
@@ -3,12 +3,16 @@ package com.thoughtworks.sbtBestPractice.git
 import org.eclipse.jgit.lib.RepositoryBuilder
 import sbt.Keys._
 import sbt._
+import sbt.plugins.CorePlugin
+
 import scala.collection.JavaConverters._
 
 /**
   * Detect GIT directories from git log
   */
 object Git extends AutoPlugin {
+
+  override def requires = CorePlugin
 
   override def trigger = allRequirements
 
@@ -25,29 +29,7 @@ object Git extends AutoPlugin {
       builder
     },
     gitWorkTree := Option(gitRepositoryBuilder.value.getWorkTree),
-    gitDir := Option(gitRepositoryBuilder.value.getGitDir),
-    developers ++= {
-      if (gitDir.value.isDefined) {
-        val repository = gitRepositoryBuilder.value.build
-        try {
-          val git = org.eclipse.jgit.api.Git.wrap(repository)
-          try {
-            (for {
-              commit <- git.log().call().asScala
-            } yield {
-              val author = commit.getAuthorIdent
-              Developer("", author.getName, author.getEmailAddress, new java.net.URL("mailto", null, author.getEmailAddress))
-            }).toSet.toList
-          } finally {
-            git.close()
-          }
-        } finally {
-          repository.close()
-        }
-      } else {
-        Nil
-      }
-    }
+    gitDir := Option(gitRepositoryBuilder.value.getGitDir)
   )
 
 }
