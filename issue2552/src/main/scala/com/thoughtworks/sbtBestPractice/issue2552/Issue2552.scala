596ba2455eb47736f958f00d0e9cf52011ac8f8d
杨博 (Yang Bo) <pop.atry@gmail.com>
2017-03-30 16:19:11 +0800

    Issue2552 should be enabled automatically

--- issue2552/src/main/scala/com/thoughtworks/sbtBestPractice/issue2552/Issue2552.scala
+++ issue2552/src/main/scala/com/thoughtworks/sbtBestPractice/issue2552/Issue2552.scala
@@ -7,7 +7,13 @@ import sbt.{AutoPlugin, Def, SimpleFileFilter}
   * @see [[https://github.com/sbt/sbt/issues/2552]]
   */
 object Issue2552 extends AutoPlugin {
+
+  override def requires = JvmPlugin
+
+  override def trigger: PluginTrigger = allRequirements
+
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     includeFilter in unmanagedSources := (includeFilter in unmanagedSources).value && new SimpleFileFilter(_.isFile)
   )
+
 }
