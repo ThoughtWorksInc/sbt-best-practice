51a83f90b427c030dcb07b4775423ac4cba12c1a
Yang Bo <atryyang@thoughtworks.com>
2017-06-14 14:03:52 +0800

    Use unidoc as the source of packageDoc task for all projects

--- publish-unidoc/src/main/scala/com/thoughtworks/sbtBestPractice/publishUnidoc/StandaloneUnidoc.scala
+++ publish-unidoc/src/main/scala/com/thoughtworks/sbtBestPractice/publishUnidoc/StandaloneUnidoc.scala
@@ -1,20 +1,17 @@
 package com.thoughtworks.sbtBestPractice.publishUnidoc
 
-import com.thoughtworks.sbtBestPractice.travis.Travis
 import com.thoughtworks.sbtBestPractice.scalacOptions.ScaladocTitle
 import sbt._
 import sbt.Keys._
 import sbt.plugins.JvmPlugin
-import sbtunidoc.Plugin.UnidocKeys
 
 object StandaloneUnidoc extends AutoPlugin {
 
   override def requires: Plugins = JvmPlugin && ScaladocTitle
 
   override def projectSettings = sbtunidoc.Plugin.scalaJavaUnidocSettings ++ Seq(
-    doc in Compile := (UnidocKeys.unidoc in Compile).value.head,
-    publishArtifact in packageSrc := false,
-    publishArtifact in packageBin := false
+    PackageUnidoc.autoImport.unidocProject in ThisBuild := Some(thisProjectRef.value),
+    publishArtifact := false
   )
 
 }
