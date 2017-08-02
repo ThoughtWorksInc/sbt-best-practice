1d290a89f952787b59d076c41b17db20ce15134c
Yang Bo <pop.atry@gmail.com>
2017-08-03 01:00:24 +0800

    Add apiURL setting for sonatype projects

--- sonatype/src/main/scala/com/thoughtworks/sbtBestPractice/sonatype/SonatypeRelease.scala
+++ sonatype/src/main/scala/com/thoughtworks/sbtBestPractice/sonatype/SonatypeRelease.scala
@@ -3,6 +3,7 @@ package com.thoughtworks.sbtBestPractice.sonatype
 import com.typesafe.sbt.SbtPgp
 import com.typesafe.sbt.pgp.PgpKeys
 import sbt._
+import sbt.Keys._
 import sbtrelease.ReleasePlugin
 import sbtrelease.ReleasePlugin.autoImport._
 import xerial.sbt.Sonatype
@@ -19,6 +20,20 @@ object SonatypeRelease extends AutoPlugin {
       releaseProcess.value.patch(releaseProcess.value.indexOf(pushChanges),
                                  Seq[ReleaseStep](releaseStepCommand(Sonatype.SonatypeCommand.sonatypeRelease)),
                                  0)
+    },
+    apiURL := {
+      val projectId = projectID.value
+      val fullVersion = scalaVersion.value
+      val binaryVersion = scalaBinaryVersion.value
+      val makeArtifactName = (artifactName in packageDoc).value
+      val docArtifact = (artifact in packageDoc).value
+      val fileName = makeArtifactName(ScalaVersion(fullVersion, binaryVersion), projectId, docArtifact)
+      val crossName = CrossVersion(projectId.crossVersion, fullVersion, binaryVersion).getOrElse(identity[String] _)
+      val moduleName = crossName(projectId.name)
+      val organizationName = projectId.organization.replace('.', '/')
+
+      Some(new URL(
+        raw"""https://oss.sonatype.org/service/local/repositories/public/archive/$organizationName/$moduleName/${projectId.revision}/$fileName/!/index.html"""))
     }
   )
 
