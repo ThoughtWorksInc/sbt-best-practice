5a67fa32630527e73c9b05355689c58ef258c583
Yang Bo <atryyang@thoughtworks.com>
2019-03-12 11:55:24 +0800

    Set-up sbt-release-early

---
+++ project/SonatypeReleaseEarly.scala
@@ -0,0 +1,15 @@
+import sbt.{AutoPlugin, Def}
+import ch.epfl.scala.sbt.release.AutoImported._
+import ch.epfl.scala.sbt.release.ReleaseEarlyPlugin
+import xerial.sbt.Sonatype
+
+/**
+  * @author 杨博 (Yang Bo)
+  */
+object SonatypeReleaseEarly extends AutoPlugin {
+  override def trigger = allRequirements
+  override def requires = Sonatype && ReleaseEarlyPlugin
+  override def projectSettings: Seq[Def.Setting[_]] = Seq(
+    releaseEarlyWith := SonatypePublisher,
+  )
+}
