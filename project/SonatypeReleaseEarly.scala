50560203d1b75ca8763f46026a0486cb8dc12fa7
Yang Bo <atryyang@thoughtworks.com>
2019-03-12 14:57:51 +0800

    Workaround for https://github.com/scalacenter/sbt-release-early/issues/30

--- project/SonatypeReleaseEarly.scala
+++ project/SonatypeReleaseEarly.scala
@@ -1,6 +1,8 @@
-import sbt.{AutoPlugin, Def}
+import sbt._
 import ch.epfl.scala.sbt.release.AutoImported._
-import ch.epfl.scala.sbt.release.ReleaseEarlyPlugin
+import ch.epfl.scala.sbt.release.{Feedback, ReleaseEarly, ReleaseEarlyPlugin}
+import com.typesafe.sbt.SbtPgp.autoImportImpl.PgpKeys
+import sbt.Keys.state
 import xerial.sbt.Sonatype
 
 /**
@@ -9,7 +11,9 @@ import xerial.sbt.Sonatype
 object SonatypeReleaseEarly extends AutoPlugin {
   override def trigger = allRequirements
   override def requires = Sonatype && ReleaseEarlyPlugin
+
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     releaseEarlyWith := SonatypePublisher,
+    Keys.aggregate in releaseEarly := false,
   )
 }
