8c8944da7b0cfa04ced2c47d835a32f710a5f33d
Yang, Bo <pop.atry@gmail.com>
2021-11-02 14:07:30 -0700

    Don't trigger Travis plugin if TRAVIS_REPO_SLUG does not exist

--- travis/src/main/scala/com/thoughtworks/sbtBestPractice/travis/Travis.scala
+++ travis/src/main/scala/com/thoughtworks/sbtBestPractice/travis/Travis.scala
@@ -12,9 +12,11 @@ import sbt._
 object Travis extends AutoPlugin {
 
   val travisBranch = settingKey[String](
-    "For builds not triggered by a pull request this is the name of the branch currently being built; whereas for builds triggered by a pull request this is the name of the branch targeted by the pull request (in many cases this will be master).")
+    "For builds not triggered by a pull request this is the name of the branch currently being built; whereas for builds triggered by a pull request this is the name of the branch targeted by the pull request (in many cases this will be master)."
+  )
   val travisRepoSlug = settingKey[String](
-    "The slug (in form: owner_name/repo_name) of the repository currently being built. (for example, “travis-ci/travis-build”).")
+    "The slug (in form: owner_name/repo_name) of the repository currently being built. (for example, “travis-ci/travis-build”)."
+  )
   val travisCommit = settingKey[String]("The commit that the current build is testing.")
 
   private val Variables = Seq(
@@ -35,6 +37,12 @@ object Travis extends AutoPlugin {
     }
   }
 
-  override def trigger = allRequirements
+  override def trigger = {
+    if (sys.env.isDefinedAt("TRAVIS_REPO_SLUG")) {
+      allRequirements
+    } else {
+      noTrigger
+    }
+  }
 
 }
