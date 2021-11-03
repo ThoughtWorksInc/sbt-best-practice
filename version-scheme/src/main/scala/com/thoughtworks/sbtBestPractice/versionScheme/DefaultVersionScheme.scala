726d8f95859e3baa8e2ce7248e18986f2729859d
Yang, Bo <pop.atry@gmail.com>
2021-11-02 19:10:16 -0700

    Add DefaultVersionScheme plugin

---
+++ version-scheme/src/main/scala/com/thoughtworks/sbtBestPractice/versionScheme/DefaultVersionScheme.scala
@@ -0,0 +1,9 @@
+package com.thoughtworks.sbtBestPractice.versionScheme
+
+import sbt.AutoPlugin
+import sbt._, Keys._
+
+object DefaultVersionScheme extends AutoPlugin {
+  override def trigger = allRequirements
+  override def buildSettings = Seq(ThisBuild / versionScheme := Some(VersionScheme.SemVerSpec))
+}
